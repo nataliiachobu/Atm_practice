46a1b714e8f224dfabc7812af810e5288902a6b1
nataliiachobu <104651560+nataliiachobu@users.noreply.github.com>
2023-05-12 09:37:00 -0700

    Create NewCustomer.java

---
+++ src/core/NewCustomer.java
@@ -0,0 +1,9 @@
+package core;
+public class NewCustomer{
+private String name,lastName;
+private int age;
+private String address;
+// create constructor for this class on your local branch.Thanks
+
+
+}
