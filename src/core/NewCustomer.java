e81fb04ee32a6de7f8dd217db0588cbc925b2c33
nataliiachobu <104651560+nataliiachobu@users.noreply.github.com>
2023-05-12 09:41:02 -0700

    Update NewCustomer.java

--- src/core/NewCustomer.java
+++ src/core/NewCustomer.java
@@ -4,6 +4,6 @@ private String name,lastName;
 private int age;
 private String address;
 // create constructor for this class on your local branch.Thanks
-
+//I forgot to add that you need to create setter and getter as well and toString
 
 }
